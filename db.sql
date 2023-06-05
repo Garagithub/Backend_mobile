10e1b56d22ddce5822019015fdee70c5c33c7611
lunaveira <naveiralucia@gmail.com>
2023-06-05 18:30:19 -0300

    lucia

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "pelicula" (
-  "id" integer PRIMARY KEY,
+  "id" serial PRIMARY KEY,
   "titulo" varchar,
   "descripcion" varchar,
   "genero" varchar,
@@ -8,8 +8,34 @@ CREATE TABLE "pelicula" (
   "imagen" varchar
 );
 
+CREATE TABLE "empresa" (
+    "id" serial PRIMARY KEY,
+    "nombre" varchar,
+    "imagen" varchar
+)
+
+
+CREATE TABLE "comentarios" (
+  "id_comentario" serial PRIMARY KEY,
+  "rating" integer,
+  "comentario" varchar,
+  "id_user" integer FOREIGN KEY usuario(id),
+  "id_pelicula" integer 
+  FOREIGN KEY (id_pelicula) references pelicula(id)
+  FOREIGN KEY (id_user) references usuario(id)
+);
+
+CREATE TABLE "usuario" (
+  "id" serial PRIMARY KEY,
+  "nombre" varchar,
+  "apellido" varchar,
+  "imagen" varchar
+);
+
+
+
 CREATE TABLE "sucursal" (
-  "id" integer PRIMARY KEY,
+  "id" serial PRIMARY KEY,
   "nombre" varchar,
   "pais" varchar,
   "provincia" varchar,
@@ -18,7 +44,14 @@ CREATE TABLE "sucursal" (
   "altura" integer,
   "precio_por_funcion" float,
   "cerrado_temporalmente" boolean,
-  id_empresa integer FOREIGN KEY empresa(id)
+  id_empresa integer, 
+  FOREIGN KEY (id_empresa) references empresa(id)
+);
+
+CREATE TABLE sala (
+  "id" serial PRIMARY KEY,
+  "id_sucursal" integer,
+  FOREIGN KEY (id_sucursal) references sucursal(id)
 );
 
 
@@ -30,71 +63,44 @@ CREATE TABLE "funcion" (
   "dia" date,
   "horario" time,
   "id_pelicula" integer,
-  "id_sala" integer
+  "id_sala" integer ,
+  FOREIGN KEY (id_pelicula) references pelicula(id),
+  FOREIGN KEY (id_sala) references sala(id)
 );
 
 CREATE TABLE "reservas" (
-  "id" integer PRIMARY KEY,
-  "id_funcion" integer,
+  "id" serial PRIMARY KEY,
+  "id_funcion" integer, 
   "id_user" integer,
   "cantidad_entradas" integer
+  FOREIGN KEY (id_funcion) references funcion(id),
+  FOREIGN KEY (id_user) references usuario(id)
+  
 );
 
-CREATE TABLE sala (
-  "id" serial PRIMARY KEY,
-  "id_sucursal" integer
-);
+
 
 CREATE TABLE asientos (
     id serial PRIMARY KEY,
     nro_asiento integer,
-    id_sala integer FOREIGN KEY sala(id),
-    reservada boolean
+    reservada boolean,
+    id_sala integer,
+    FOREIGN KEY (id_sala) references sala(id),
+    
 )
 
 CREATE TABLE "socio" (
-  "id_socio" integer PRIMARY KEY,
+  "id_socio" serial PRIMARY KEY,
   "email" varchar,
   "password" varchar,
-  "id_empresa" integer FOREIGN KEY "empresa"("id")
+  "id_empresa" integer,
+  FOREIGN KEY (id_empresa) references "empresa"("id")
 );
 
-CREATE TABLE "empresa" (
-    "id" integer PRIMARY KEY,
-    "nombre" varchar,
-    "imagen" varchar
-)
 
-CREATE TABLE "usuario" (
-  "id_user" integer PRIMARY KEY,
-  "nombre" varchar,
-  "apellido" varchar,
-  "imagen" varchar
-);
 
-CREATE TABLE "comentarios" (
-  "id_comentario" integer PRIMARY KEY,
-  "rating" integer,
-  "comentario" varchar,
-  "id_user" integer,
-  "id_pelicula" integer
-);
 
-ALTER TABLE "sala" ADD FOREIGN KEY ("id_sucursal") REFERENCES "sucursal" ("id");
 
-ALTER TABLE "pelicula" ADD FOREIGN KEY ("id") REFERENCES "funcion" ("id_pelicula");
-
-ALTER TABLE "funcion" ADD FOREIGN KEY ("id") REFERENCES "reservas" ("id_funcion");
-
-ALTER TABLE "sucursal" ADD FOREIGN KEY ("id") REFERENCES "socio" ("id_sucursal");
-
-ALTER TABLE "sala" ADD FOREIGN KEY ("id") REFERENCES "funcion" ("id_sala");
-
-ALTER TABLE "usuario" ADD FOREIGN KEY ("id_user") REFERENCES "reservas" ("id_user");
-
-ALTER TABLE "comentarios" ADD FOREIGN KEY ("id_user") REFERENCES "usuario" ("id_user");
-
-ALTER TABLE "pelicula" ADD FOREIGN KEY ("id") REFERENCES "comentarios" ("id_pelicula");
 
 
 
