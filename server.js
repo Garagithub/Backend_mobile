04985fceed7e7452453809df5a49f5ca21dcabf8
Garagithub <119074045+Garagithub@users.noreply.github.com>
2023-06-05 16:06:30 -0300

    cambios

--- server.js
+++ server.js
@@ -94,6 +94,57 @@ server.post("/api/users",async (req,res)=>{
 catch(er){res.sendStatus(500)}
 })
 
+server.post("/api/cinema/{id_cinema}/branches", async (req, res) => {
+  try {
+  const { id, nombre, pais, provincia, localidad, calle, altura, precio, cerrado } = req.body;
+  
+  if (!id || typeof(id) !== 'number' || !nombre || typeof(nombre) !== 'string' || !pais || typeof(pais) !== 'string' ||
+      !provincia || typeof(provincia) !== 'string' || !localidad || typeof(localidad) !== 'string' ||
+      !calle || typeof(calle) !== 'string' || !altura || typeof(altura) !== 'number' ||
+      !precio || typeof(precio) !== 'number' || typeof(cerrado) !== 'boolean') {
+    res.sendStatus(400);
+    return;
+  }
+  const sucursalexiste= await db.query("select nombre from sucursal where nombre=$1", [nombre])
+  if (sucursalexiste.rows.length>=1 ){
+  
+    res.status(400).send("There is already a branch with this name");
+
+    return;
+  }
+  
+    const sucursal = await db.query("INSERT INTO sucursal (id, nombre, pais, provincia, localidad, calle, altura, precio, cerrado) " +
+                                    "VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+                                    [id, nombre, pais, provincia, localidad, calle, altura, precio, cerrado]);
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+server.delete("/api/cinema/{id_cinema}/branches/{id_branch}", async (req,res)=>{
+  const id_sucursal = req.params.id_sucursal;
+
+  try {
+    //revisar este try esta raro
+   const existesucursal= await db.query("select nombre from sucursal where id=$1", [id_sucursal])
+   if (existesucursal.rows.length===0 ){
+   
+     res.status(400).send("There is no branch whit this id");
+ 
+     return;
+   }
+    await db.query('DELETE FROM sucursal WHERE id = $1', [id_sucursal]);
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error('Error al eliminar la sucursal:', error);
+    res.sendStatus(500);
+  }
+});
+
+
 server.listen(port,()=> console.log('El servidor está escuchando en localhost: '+ port));
 
 
