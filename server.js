dc17b7c89e8b941d5a664ccd098b6e75a7442eee
Garagithub <119074045+Garagithub@users.noreply.github.com>
2023-06-05 22:15:06 -0300

    CRUDfunciones, peliculas y salas en teoria

--- server.js
+++ server.js
@@ -98,7 +98,58 @@ const {id_sucursal, fila, columna}= req.body;
 
 })
 
-server.put()
+server.put('/cinema-room/:id_sala', async (req, res) => {
+  try {
+    const { id_sala } = req.params;
+    const { fila, columna } = req.body;
+
+    if (!id_sala || typeof(id_sala) !== 'number' || !fila || typeof(fila) !== 'number' ||
+        !columna || typeof(columna) !== 'number') {
+      res.sendStatus(400);
+      return;
+    }
+
+    const sala = await db.query('SELECT * FROM sala WHERE id = $1', [id_sala]);
+
+    if (sala.rows.length === 0) {
+      res.status(404).send('Cinema room not found');
+      return;
+    }
+
+    // Realizar la lógica de actualización de la sala de cine aquí
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+server.delete('/cinema-room/:id_sala', async (req, res) => {
+  try {
+    const { id_sala } = req.params;
+
+    if (!id_sala || typeof(id_sala) !== 'number') {
+      res.sendStatus(400);
+      return;
+    }
+
+    const sala = await db.query('SELECT * FROM sala WHERE id = $1', [id_sala]);
+
+    if (sala.rows.length === 0) {
+      res.status(404).send('Cinema room not found');
+      return;
+    }
+
+    // Realizar la lógica de eliminación de la sala de cine aquí
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
 
 
 server.post("/api/users",async (req,res)=>{
@@ -177,6 +228,198 @@ server.delete("/api/cinema/{id_cinema}/branches/{id_branch}", async (req,res)=>{
     res.sendStatus(200);
   } catch (error) {
     console.error('Error al eliminar la sucursal:', error);
+    res.sendStatus(500);
+  }
+});
+
+server.put("/api/cinema/{id_cinema}/branches/{id_branch}", async (req, res) => {
+  try {
+    const { id_branch } = req.params;
+    const { id, nombre, pais, provincia, localidad, calle, altura, precio, cerrado } = req.body;
+
+    if (!id || typeof(id) !== 'number' || !nombre || typeof(nombre) !== 'string' || !pais || typeof(pais) !== 'string' ||
+        !provincia || typeof(provincia) !== 'string' || !localidad || typeof(localidad) !== 'string' ||
+        !calle || typeof(calle) !== 'string' || !altura || typeof(altura) !== 'number' ||
+        !precio || typeof(precio) !== 'number' || typeof(cerrado) !== 'boolean') {
+      res.sendStatus(400);
+      return;
+    }
+
+    const sucursalexiste = await db.query("SELECT nombre FROM sucursal WHERE nombre=$1", [nombre]);
+    if (sucursalexiste.rows.length >= 1) {
+      res.status(400).send("There is already a branch with this name");
+      return;
+    }
+
+    const sucursal = await db.query("UPDATE sucursal SET id = $1, nombre = $2, pais = $3, provincia = $4, " +
+                                    "localidad = $5, calle = $6, altura = $7, precio = $8, cerrado = $9 " +
+                                    "WHERE id_branch = $10 RETURNING *",
+                                    [id, nombre, pais, provincia, localidad, calle, altura, precio, cerrado, id_branch]);
+
+    if (sucursal.rows.length === 0) {
+      res.status(404).send("Branch not found");
+      return;
+    }
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+server.post('/api/peliculas', async (req, res) => {
+  try {
+    const { titulo, descripcion, genero, promedio_calificacion, en_cines, imagen } = req.body;
+
+    if (!titulo || !descripcion || !genero || typeof promedio_calificacion !== 'number' || typeof en_cines !== 'boolean' || !imagen) {
+      res.sendStatus(400);
+      return;
+    }
+
+    const nuevaPelicula = await db.query(
+      'INSERT INTO pelicula (titulo, descripcion, genero, promedio_calificacion, en_cines, imagen) ' +
+      'VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+      [titulo, descripcion, genero, promedio_calificacion, en_cines, imagen]
+    );
+
+    res.status(201).json(nuevaPelicula.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+server.put('/api/peliculas/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { titulo, descripcion, genero, promedio_calificacion, en_cines, imagen } = req.body;
+
+    if (!titulo || !descripcion || !genero || typeof promedio_calificacion !== 'number' || typeof en_cines !== 'boolean' || !imagen) {
+      res.sendStatus(400);
+      return;
+    }
+
+    const peliculaActualizada = await db.query(
+      'UPDATE pelicula SET titulo=$1, descripcion=$2, genero=$3, promedio_calificacion=$4, en_cines=$5, imagen=$6 ' +
+      'WHERE id=$7 RETURNING *',
+      [titulo, descripcion, genero, promedio_calificacion, en_cines, imagen, id]
+    );
+
+    if (peliculaActualizada.rows.length === 0) {
+      res.sendStatus(404);
+      return;
+    }
+
+    res.json(peliculaActualizada.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+server.delete('/api/peliculas/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const peliculaEliminada = await db.query('DELETE FROM pelicula WHERE id=$1 RETURNING *', [id]);
+
+    if (peliculaEliminada.rows.length === 0) {
+      res.sendStatus(404);
+      return;
+    }
+
+    res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+server.post('/api/funciones', async (req, res) => {
+  try {
+    const { dia, horario, id_pelicula, id_sala } = req.body;
+
+    // Validar los campos requeridos y tipos de datos
+    if (!dia || !horario || typeof id_pelicula !== 'number' || typeof id_sala !== 'number') {
+      res.sendStatus(400);
+      return;
+    }
+
+    // Verificar si la película y la sala existen en la base de datos
+    const peliculaExiste = await db.query('SELECT * FROM pelicula WHERE id = $1', [id_pelicula]);
+    const salaExiste = await db.query('SELECT * FROM sala WHERE id = $1', [id_sala]);
+
+    if (peliculaExiste.rows.length === 0 || salaExiste.rows.length === 0) {
+      res.sendStatus(404);
+      return;
+    }
+
+    const nuevaFuncion = await db.query(
+      'INSERT INTO funcion (dia, horario, id_pelicula, id_sala) ' +
+      'VALUES ($1, $2, $3, $4) RETURNING *',
+      [dia, horario, id_pelicula, id_sala]
+    );
+
+    res.status(201).json(nuevaFuncion.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+server.put('/api/funciones/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { dia, horario, id_pelicula, id_sala } = req.body;
+
+    // Validar los campos requeridos y tipos de datos
+    if (!dia || !horario || typeof id_pelicula !== 'number' || typeof id_sala !== 'number') {
+      res.sendStatus(400);
+      return;
+    }
+
+    // Verificar si la película y la sala existen en la base de datos
+    const peliculaExiste = await db.query('SELECT * FROM pelicula WHERE id = $1', [id_pelicula]);
+    const salaExiste = await db.query('SELECT * FROM sala WHERE id = $1', [id_sala]);
+
+    if (peliculaExiste.rows.length === 0 || salaExiste.rows.length === 0) {
+      res.sendStatus(404);
+      return;
+    }
+
+    const funcionActualizada = await db.query(
+      'UPDATE funcion SET dia=$1, horario=$2, id_pelicula=$3, id_sala=$4 ' +
+      'WHERE id=$5 RETURNING *',
+      [dia, horario, id_pelicula, id_sala, id]
+    );
+
+    if (funcionActualizada.rows.length === 0) {
+      res.sendStatus(404);
+      return;
+    }
+
+    res.json(funcionActualizada.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+server.delete('/api/funciones/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const funcionEliminada = await db.query('DELETE FROM funcion WHERE id=$1 RETURNING *', [id]);
+
+    if (funcionEliminada.rows.length === 0) {
+      res.sendStatus(404);
+      return;
+    }
+
+    res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
     res.sendStatus(500);
   }
 });
