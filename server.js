d8ff565a33fcaceaf97f51964e43a01b870ca798
Garagithub <119074045+Garagithub@users.noreply.github.com>
2023-07-06 18:57:36 -0300

    ss

--- server.js
+++ server.js
@@ -325,6 +325,26 @@ server.delete('/:idsucursal/:cinema-room/deletecinemaroom', async (req, res) => 
     res.sendStatus(500);
   }
 });
+server.delete('/:idsucursal/:cinema_room/deletecinemarooms', async (req, res) => {
+  try {
+    const { idsucursal, cinema_room } = req.params;
+
+    const sala = await db.query('SELECT id FROM salas WHERE id_sucursal = $1 AND numero_sala = $2', [idsucursal, cinema_room]);
+
+    if (sala.rows.length === 0) {
+      res.status(404).send('Cinema room not found');
+      return;
+    }
+
+    const eliminar_asientos = await db.query('DELETE FROM asientos WHERE id_sala = $1', [sala.rows[0].id]);
+    const eliminacion = await db.query('DELETE FROM salas WHERE id = $1', [sala.rows[0].id]);
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
 
 server.get('/:id_sucursal/cinema-room/getall',async (req, res)=>{
   try{
